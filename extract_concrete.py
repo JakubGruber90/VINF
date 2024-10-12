43b8232a7cfc230ba39b2862042db5c3cb4d9440
Jakub Grúber <jakubgruber90@gmail.com>
2024-10-12 23:03:54 +0200

    vylepseny extraktor

--- extract_concrete.py
+++ extract_concrete.py
@@ -1,12 +1,4 @@
 import re
-import os
-import json
-
-CURR_DIR = os.getcwd()
-TEXT_DATA_PATH = os.path.join(CURR_DIR, 'data', 'data_text')
-HTML_DATA_PATH = os.path.join(CURR_DIR, 'data', 'data_raw')
-JSON_DATA_PATH = os.path.join(CURR_DIR, 'data', 'data_json')
-TEXT_DATA_DIR = os.fsencode(TEXT_DATA_PATH)
 
 ### Functions to extract key info with regex
 def get_released(file):
@@ -48,7 +40,7 @@ def get_genre(file):
         genres = re.findall(pattern, result)
         return [genre.strip() for genre in genres]
     else:
-        return 'N/A'   
+        return 'N/A'  
 
 def get_perspective(file):
     match = re.search(r'Perspective (.*?)\s*(?=Gameplay|Interface|Setting|Narrative|Misc)', file)
@@ -60,7 +52,7 @@ def get_perspective(file):
         perspectives = re.findall(pattern, result)
         return [perspective.strip() for perspective in perspectives]
     else:
-        return 'N/A' 
+        return 'N/A'  
 
 def get_gameplay(file):
     match = re.search(r'Gameplay (.*?)\s*(?=Interface|Setting|Narrative|Misc)', file)
@@ -101,34 +93,26 @@ def get_description(file):
         return 'N/A'
 
 ### Main
-if __name__ == '__main__': 
-    for file in os.listdir(TEXT_DATA_DIR):
-        filename = os.fsdecode(file).split('.')[0]
-        
-        text_file_path = os.path.join(TEXT_DATA_PATH, filename+'.txt')
-        html_file_path = os.path.join(HTML_DATA_PATH, filename+'.html')
-        json_file = os.path.join(JSON_DATA_PATH, filename+'.json')
-        
-        with open(text_file_path, 'r', encoding='utf-8') as r:
-            text_file = r.read()
-        
-        text_clean = ' '.join(text_file.split())  # Strip text file of unwanted whitespace characters for easier regex
-        
-        with open(html_file_path, 'r', encoding='utf-8') as r:
-            html_file = r.read()
-        
-        data_dict = {
-            'Released': get_released(text_clean),
-            'Publishers': get_publishers(text_clean),
-            'Developers': get_developers(text_clean),
-            'Moby Score': get_moby_score(text_clean),
-            'Critics Score': get_critics_score(text_clean),
-            'Players Score': get_players_score(html_file),
-            'Genre': get_genre(text_clean),
-            'Perspective': get_perspective(text_clean),
-            'Gameplay': get_gameplay(text_clean),
-            'Description': get_description(text_clean)
-        }
-        
-        with open(json_file, 'w') as j:
-            json.dump(data_dict, j)
+if __name__ == '__main__':
+    with open('elden-ring.txt', 'r', encoding='utf-8') as r:
+        text_file = r.read()
+
+    text_clean = ' '.join(text_file.split())  # Strip text file of unwanted whitespace characters for easier regex
+
+    with open('elden-ring.html', 'r', encoding='utf-8') as r:
+        html_file = r.read()
+
+    data_dict = {
+        'Released': get_released(text_clean),
+        'Publishers': get_publishers(text_clean),
+        'Developers': get_developers(text_clean),
+        'Moby Score': get_moby_score(text_clean),
+        'Critics Score': get_critics_score(text_clean),
+        'Players Score': get_players_score(html_file),
+        'Genre': get_genre(text_clean), #list
+        'Perspective': get_perspective(text_clean), #list
+        'Gameplay': get_gameplay(text_clean), #list
+        'Description': get_description(text_clean)
+    }
+    
+    print(data_dict)
